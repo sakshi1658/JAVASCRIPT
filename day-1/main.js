e9738582ecd18ee41a4b28718727e423fc4420d5
Sakshi Sharma <sharmasakshi1658@gmail.com>
2024-08-24 14:57:22 +0530

    var and datatypes

--- day-1/main.js
+++ day-1/main.js
@@ -1,1 +1,11 @@
+//Log a statemnetusing console.log()
 console.log("Hello from main.js");
+
+// Variables Let & Const
+let age = 22
+age = 23  // Can Reassign value
+console.log(age)
+
+const salary = 35000
+salary = 40000 // Can't reassign value
+console.log(salary)
