b5f2ba93cff3300bfcd4effda644c2fc44f377d5
Sakshi Sharma <sharmasakshi1658@gmail.com>
2024-08-25 13:38:59 +0530

    questions

--- day-1/main.js
+++ day-1/main.js
@@ -2,10 +2,46 @@
 console.log("Hello from main.js");
 
 // Variables Let & Const
-let age = 22
-age = 23  // Can Reassign value
-console.log(age)
+let age = 22;
+age = 23; // Can Reassign value
+console.log(age);
 
-const salary = 35000
-salary = 40000 // Can't reassign value
-console.log(salary)
+const salary = 35000;
+// Can't reassign value salary = 40000
+console.log(salary);
+
+
+//  Datatypes
+let x = 10;
+console.log(x);
+
+const username = "Sakshi Sharma";
+console.log(username);
+
+let isUsername = true;
+
+let person = null;
+
+let y;
+
+let user = { name: "Sakshi", age: 22 };
+user.name = "Nurul";
+user.age = 24;
+console.log(user);
+
+// QUESTIONS
+
+// Declare three variables a, b, and c in a single line. Assign them values 5, 10, and 15 respectively.
+
+let a= 1, b= 2, c= 3;
+
+// Declare a variable x and assign it the value 10. Then, change its value to 20.
+
+let g = 10;
+g = 20; 
+console.log(g);
+
+// Declare a variable isAvailable and assign it a boolean value indicating whether a product is available.
+
+let isAvailable = true;
+console.log(isAvailable);
