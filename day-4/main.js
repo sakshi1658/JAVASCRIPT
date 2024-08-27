f8a17ba57338b464561110dbad831f7579f196f4
Sakshi Sharma <sharmasakshi1658@gmail.com>
2024-08-27 11:50:15 +0530

    questions added

---
+++ day-4/main.js
@@ -0,0 +1,11 @@
+// conditional statement basic questions?
+// 1. What is the condition in a conditional statement? 
+// 2. What are the basic types of conditional statements?
+// 3. How do you write a conditional statement in a programming language?
+// 4. What is the purpose of a conditional statement?
+// 5. What are some common uses of conditional statements?
+// 6. How do you handle multiple conditions in a conditional statement?
+// 7. What is the difference between an if statement and an if-else statement?
+// 8. How do you use a conditional statement to make decisions in a program?
+// 9. What are some best practices for using conditional statements?
+// 10.  How do you debug conditional statements in a program?
