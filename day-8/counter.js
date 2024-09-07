35d2a75b3d3c2395f676b03687fe0aa9e77fa076
Sakshi Sharma <sharmasakshi1658@gmail.com>
2024-09-07 16:49:47 +0530

    counter-app

---
+++ day-8/counter.js
@@ -0,0 +1,18 @@
+let count = 0;
+
+document.getElementById("increment-btn").onclick = function(){
+    count+=1;
+    document.getElementById("counter-value").innerHTML = count;
+}
+
+document.getElementById("decrement-btn").onclick = function(){
+    count-=1;
+    document.getElementById("counter-value").innerHTML = count;
+}
+
+document.getElementById("reset-btn").onclick = function(){
+    count=0;
+    document.getElementById("counter-value").innerHTML = count;
+}
+
+
