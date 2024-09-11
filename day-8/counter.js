8171f1ae98383bae882100c0e58afc7579eba829
Sakshi Sharma <sharmasakshi1658@gmail.com>
2024-09-11 11:36:00 +0530

    define some inbulit function and event

--- day-8/counter.js
+++ day-8/counter.js
@@ -5,12 +5,22 @@ document.getElementById("increment-btn").onclick = function(){
     document.getElementById("counter-value").innerHTML = count;
 }
 
-document.getElementById("decrement-btn").onclick = function(){
-    if (count>0) {
-        count-=1
-    }
-    document.getElementById("counter-value").innerHTML = count;
+//  here we are making function and  calling it on button click with onclick  event 
+function  decrement(){
+    count-=1;
+    document.getElementById("counter-value").innerHTML = count; // this update the  value of counter with help of innerHTml () method
+// document allow us  to access the element of the webpage & getelement byid() method is used to access the element of the webpage by id
+
 }
+
+
+// document.getElementById("decrement-btn").onclick = function(){
+//     if (count==0) {
+//         return 0;
+//     }
+//     count-=1
+//     document.getElementById("counter-value").innerHTML = count;
+// }
 
 document.getElementById("reset-btn").onclick = function(){
     count=0;
