03dfbe45c0dd437958b9e9fb06425b876be9349c
Sakshi Sharma <sharmasakshi1658@gmail.com>
2024-12-06 18:58:12 +0530

    function to show submitted blogs

--- blog-app/script.js
+++ blog-app/script.js
@@ -1,7 +1,9 @@
 const blogs = []
 
 const titleEl = document.getElementById('title');
-const content = document.getElementById('content');
+const contentEl = document.getElementById('content');
+const blogsEl = document.getElementById('blogs');
+
 
 function getBlogs() {
     const tempBlogs = localStorage.getItem("blogs");
@@ -18,14 +20,30 @@ function setBlogs(blog) {
 }
 
 function onSubmit(){
+    const id = Date.now()
     const newBlog = {
+        id,
         title: titleEl.value,
-        content: content.value
+        content: contentEl.value
     }
-    blogs.push({
-        title: titleEl.value,
-        content: content.value
-    }) 
+    blogs.push(newBlog) 
     setBlogs(newBlog)
     console.log('Submit', getBlogs());
 }
+
+function setBlogs() {
+    const tempBlogs = getBlogs();
+
+    for(let i = 0; i < tempBlogs.length; i++) {
+        let a = document.createElement("a")
+        let h3 = document.createElement("h3")
+
+        a.setAttribute("href", "#")
+        a.setAttribute("onclick", "")
+        h3.innerHTML = tempBlogs[i].title
+        a.append(h3)
+        blogsEl.append(a);
+    }
+}
+
+setBlogs()
