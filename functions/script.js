7adc5354cc592c9da5486233c57e4b602d981f0e
Sakshi Sharma <sharmasakshi1658@gmail.com>
2024-12-30 12:42:55 +0530

    function practice

--- functions/script.js
+++ functions/script.js
@@ -5,9 +5,31 @@ function isEven(number) {
 console.log(isEven(4));
 console.log(isEven(7));
 
-
 // <!-- Reverse a String -->
 function reverse(word) {
   return word.split("").reverse().join("");
 }
 console.log(reverse("sakshi"));
+
+// Find the Largest Number in an Array
+function findLargest(arr) {
+  let largest = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+  return largest;
+}
+const numbers = [3, 6, 54, 21, 1243, 232];
+console.log(findLargest(numbers));
+
+//  Check for Palindrome
+function isPalindrome(num){
+    let numStr = num.toString();
+    let result = numStr.split('').reverse().join('');
+    return numStr === result;
+}
+console.log(isPalindrome(121));
+console.log(isPalindrome(3132));
+console.log(isPalindrome('madam'));
