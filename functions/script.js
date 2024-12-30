42fbae2b95be518a322e1a7f3e28206de594acf5
Sakshi Sharma <sharmasakshi1658@gmail.com>
2024-12-30 12:10:31 +0530

    function practice

--- functions/script.js
+++ functions/script.js
@@ -1,6 +1,13 @@
-function isEven(number){
- return number % 2 === 0;
+function isEven(number) {
+  return number % 2 === 0;
 }
 
 console.log(isEven(4));
 console.log(isEven(7));
+
+
+// <!-- Reverse a String -->
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
+console.log(reverse("sakshi"));
